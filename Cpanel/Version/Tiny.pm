5fed80f31e181318eab351a8af31d76729b6cf0b
anildalar <dollorinfotech@gmail.com>
2024-10-22 05:08:40 +0200

    Before Activation

---
+++ Cpanel/Version/Tiny.pm
@@ -0,0 +1,25 @@
+package Cpanel::Version::Tiny;
+
+#----------------------------------------------------------------------
+# XXX XXX XXX
+#
+# Do NOT use this directly!! This module’s interface is not guaranteed.
+# Use Cpanel/Version.pm instead.
+#----------------------------------------------------------------------
+
+use strict;
+
+our $VERSION         = '11.122.0';
+our $VERSION_BUILD   = '11.122.0.21';
+our $VERSION_TEXT    = '122.0 (build 21)';
+our $VERSION_DISPLAY = '122.0.21';
+
+our $parent_version = 11;
+our $major_version  = 122;
+our $minor_version  = 0;
+our $build_number   = 21;
+
+our $build_time_text = 'Wed Oct 16 10:41:07 2024';
+our $buildtime       = 1729093267;
+
+1;
